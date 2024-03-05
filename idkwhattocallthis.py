6ddd0996415068f16a857a01de9109f5c8339927
Abby Berg <audreychip@gmail.com>
2024-03-05 10:32:59 -0700

    3/5

--- idkwhattocallthis.py
+++ idkwhattocallthis.py
@@ -1,12 +1,52 @@
 #BIG ALGORITHM THAT CODES IN THAT MATRIX TABLE THING THAT WE MADE
-#LAST UPDATED 2/29 BY ABBY
+#LAST UPDATED 3/5/24 BY ABBY
+
+'''
+HEART RATE INPUT FILE FORMAT:
+    starting time stamp [integer : sec]
+    heart rate data [integer : BPM]
+    .
+    ..
+    ...
+    ending time stamp [integer : sec]
+
+VOCAL ANALYSIS INPUT FILE FORMAT:
+    current speaker [integer : 1,2,3]
+    starting time stamp [integer : sec]
+    fundamental frequency data [integer : Hz] articulation rate data [integer]
+    .
+    ..
+    ...
+    ending time stamp [integer : sec]
+'''
+
 
 import time
 import numpy
 import my-voice-analysis
 
+
 start_time = time.time();
 system_status = True;
+time_chunk = 10; #TIME TO WAIT BETWEEN ANALYSIS LOOPS
+
+
+'''
+METRICS REFLECT THE CHANGES IN THE ASSOCIATED INPUT DATA:
+   * -1 indicates a negative change
+   * 0 indicates no change
+   * 1 indicates a positive change
+'''
+hr_metric = None;
+ff_metric = None;
+ar_metric = None;
+
+
+hr_data_log = []; #ALL HEART RATE DATA
+ff_data_log = []; #ALL FUNDAMENTAL FREQUENCY DATA
+ar_data_log = []; #ALL ARTICULATION RATE DATA
+
+current_speaker = None; #CURRENT ANALYSIS SUBJECT VIA DIARIZATION
 
 hr_list = [];
 hr_avg = 0;
@@ -16,11 +56,52 @@ ar_list[];
 ar_avg = 0;
     
 
+#MAIN LOOP THAT CALLS ALL OF THE FUNCTIONS AND DOES ALL OF THE STUFF
 while system_status:
     current_time = time.time() - start_time;
     #calculate heart rate
     #calculate fundamental frequency and articulation rate
 
+    try:
+        with open('HEARTRATEFILENAME.TXT', 'r') as hr_file:
+            hr_content = hr_file.read();
+        except FileNotFoundError:
+            print("ERROR: Heart-rate input file not found.")
+
+    try:
+        with open('VOICESTUFFFILENAME.TXT', 'r') as va_file:
+            va_content = va_file.read()
+        except FileNotFoundError:
+            print("ERROR: Vocal analyis input file not found.")
+
+    hr_content.split('\n');
+    hr_content[0] = hr_start_time;
+    hr_content.pop(0)
+    hr_content[-1] = hr_end_time;
+    hr_content.pop()
+    hr_list = hr_content;
+
+    
+    va_content.split('\n');
+    va_content[0] = current_speaker;
+    va_content.pop(0)
+    va_content[0] = va_start_time;
+    va_content.pop(0)
+    va_content[-1] = va_end_time;
+    va_content.pop()
+
+    va_content.split(' ');
+    for i in range(len(va_content)):
+        int(va_content[i]);
+    
+        if i % 2 == 0:
+            ff_list.append(i);
+        else:
+            ar_list.append(i);
+
+    
+    time.sleep(time_chunk);
+        
 
 #WEIGHTS THE HEART RATE DATA
 def hr_weight():
@@ -30,14 +111,32 @@ def hr_weight():
     hr_list.append(current_hr);
     delta_hr_avg = hr_avg - prev_hr_avg;
 
+    #calculation of heart trate metric depending on changes in the average and current value
+    if delta_hr == 0:
+        hr_metric = 0;
+    elif delta_hr < 0 and delta_hr_avg < 0:
+        hr_metric = -1;
+    elif delta_hr > 0 and delta_hr_avg > 0:
+        hr_metric = 1;
+    elif delta_hr > 0 and delta_hr_avg < 0:
+        hr_metric = -1;
+    elif delta_hr < 0 and delta_hr_avg > 0:
+        hr_metric = 1;
+    else:
+        print("POTENTIAL ERROR IN delta_hr hr calculations")
+
+    return hr_metric
+
     
 #WEIGHTS THE FUNDAMENTAL FREQUENCY DATA
 def fr_weight():
 
+    return fr_metric
 
 #WEIGHTS THE ARTICULATION RATE DATA
 def ar_weight():
 
+    return ar_weight
 
 #CALCULATES THE OUTPUT OF THE MATRIX TO BE SENT TO THE HAPTIC SYSTEM DEPENDING ON THE INPUT DATA
 #FOR THE HEART RATE, FUNDAMENTAL FREQUENCY, AND ARTICULATION RATE
@@ -46,6 +145,15 @@ def matrix(hr_metric, ff_metric, ar_metric):
     #ff = fundamental frequency
     #ar = articulation rate
     #mo = matrix output
+    #va = vocal analysis
+
+    if type(hr_metric) == None:
+        raise Exception("ERROR: Heart-rate metric not defined.");
+    if type(ff_metric) == None:
+        raise Exception("ERROR: Fundamental frequency metric not defined.");
+    if type(ar_metric) == None:
+        raise Exception("Error: Articulation rate metric not defined.");
+
     
     mo = 0;
     mo_indicator = 'VALID';
@@ -91,6 +199,39 @@ def matrix(hr_metric, ff_metric, ar_metric):
             mo = -2; #SPECIAL CASE
             
     return mo;
+
+
+
+#SEND SERIAL OUTPUT TO THE HAXYLS TO NOTIFY THE USER AS INDICATED BY THE METRIC OUTPUT
+#CALCULATED ABOVE
+def haptic_output():
+    
+    if mo == -2:
+        
+    elif mo == -1:
+        
+    elif mo == 0:
+
+    elif mo == 1:
+
+    elif mo == 2:
+
+
+
+#NEED TO FIX THIS THIS SYNTAX DOESNT EXIST LOL WILL FIX LATER
+#WRITE TO FILES TO SAVE THE DATA FOR THE ENTIRE ANALYSIS RUN
+hr_f= open("heartratedata.txt", "w")
+hr_f.write(hr_data_log)
+hr_f.close()
+
+ff_f = open("fundamentalfrequency.txt","w")
+ff_f.write(ff_data_log)
+ff_f.close()
+
+ar_f = open("articulationrate.txt","w")
+ar_f.write(ar_data_log)
+ar_f.close()
+
 
 
 
